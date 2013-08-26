7a3a38b5111c611eea5b30171ff9bcf8ac620bd7
Андрей Листочкин <andrew.listochkin@gmail.com>
2013-08-26 23:04:29 +0300

    Notifications route and fixtures

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,27 @@
 App = Ember.Application.create();
 
-App.Store = DS.Store.extend();
+App.Router.map(function() {
+    this.resource('notifications');
+});
+
+App.NotificationsController = Ember.ArrayController.extend({
+    init: function () {
+        this._super();
+        this.set('content', App.Notification.find());
+    }
+});
+
+App.Store = DS.Store.extend({
+    adapter: DS.FixtureAdapter.create()
+});
+
+App.Notification = DS.Model.extend({
+    title: DS.attr('string')
+});
+
+App.Notification.FIXTURES = [
+    { id: 1, title: 'Notification 1' },
+    { id: 2, title: 'Notification 2' },
+    { id: 3, title: 'Notification 3' },
+    { id: 4, title: 'Notification 4' }
+];
--- test/js/test-notifications.js
+++ test/js/test-notifications.js
@@ -10,3 +10,11 @@ it('Should go to index route', function () {
         expect(find('[data-test="index"]')[0].innerText).to.equal('INDEX');
     });
 });
+
+describe('Notifications', function () {
+    it('Should go to /notifications', function () {
+        visit('/notifications').then(function () {
+            expect(find('[data-test="title"]').length).to.equal(4);
+        });
+    });
+});
