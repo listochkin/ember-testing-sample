69043f9b1a78042dcdc10f75344000c895a0a448
Андрей Листочкин <andrew.listochkin@gmail.com>
2013-08-26 21:46:06 +0300

    Simple test in Karma

---
+++ test/js/helpers/chai-test-helper.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var expect = chai.expect;
+chai.Assertion.includeStack = true;
---
+++ test/js/test-simple.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('A simple Mocha test', function () {
+    it('should pass', function () {
+        expect(1).to.equal(1);
+    });
+});
